a3a0f3e794f9b7bddfc2c05d39e1e063aac82576
Lev Kokotov <lev.kokotov@gmail.com>
2026-07-17 08:26:09 -0700

    remove unnecessary tests

--- tests/test_create_operator_family.rs
+++
@@ -1,8 +0,0 @@
-mod common;
-
-use common::run_parse_debug_cases as run_cases;
-
-#[test]
-fn create_operator_family_parses() {
-    run_cases(&[r#"CREATE OPERATOR FAMILY my_family USING btree"#]);
-}
