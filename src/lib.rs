273d16bc9f6d7c8251e4efe562b1e0055565f8c9
Sage Griffin <sage@sagetheprogrammer.com>
2026-08-04 14:26:36 -0600

    Add `MemoryToken::parse`
    Currently if you want to manipulate a query immediately after parsing
    it, you have to call `pg_raw_parse::parse`, then
    `MemoryToken::make_unique` to perform a deep copy onto a new memory
    context. We have places in `pgdog` which are using this pattern, and
    immediately throwing away the unmodified AST/MemoryContext. We can skip
    the extra copy/allocation by allowing you to parse within `make::try_owned`
    directly.
    
    We ended up never actually exposing the `warnings` field of
    `ParseResult`, so I've gone ahead and removed it entirely. If we do ever
    want to expose them, I'll either need to change the signature of `parse`
    to return them as well as the stmts, or provide a new function that
    returns both.
    
    At this point `ParseResult` is a pointless type and can be removed, but
    doing so is an API breaking change so I've left it in for now.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(feature = "field_offset_assertions", feature(offset_of_enum))]
-use std::{ffi, fmt, ops, ptr};
+use std::{fmt, ops};
 
 pub mod const_val;
 mod deparse;
@@ -29,36 +29,14 @@ pub(crate) use node_ptr::{
 };
 
 pub fn parse(sql: &str) -> Result<ParseResult, error::Error> {
-    let mem = mem::MemoryContext::new(c"pg_raw_parse");
-    let cstring = ffi::CString::new(sql).map_err(error::Error::StatementContainedNul)?;
-    // SAFETY: we never panic within the provided block
-    let c_result = unsafe {
-        mem.within(|| {
-            raw::pg_query_raw_parse(
-                cstring.as_ptr(),
-                raw::PgQueryParseMode::PG_QUERY_PARSE_DEFAULT as _,
-            )
-        })
-    };
-    // Any warnings that were emitted during parsing went into a malloc'd
-    // buffer, so we need to construct this even if we're going to return Err
-    // to ensure that buffer is freed.
-    let warnings = Warnings {
-        stderr_buffer: ptr::NonNull::new(c_result.stderr_buffer),
-    };
-    match ptr::NonNull::new(c_result.error) {
-        Some(e) => Err(Error::from_pg_query_error(e)),
-        None => Ok(ParseResult {
-            _warnings: warnings,
-            tree: Owned::new(mem, c_result.tree.cast()),
-        }),
-    }
+    Ok(ParseResult {
+        tree: make::try_owned(|mem| mem.parse(sql))?,
+    })
 }
 
 pub type StmtList = list::CastNodeList<nodes::RawStmt>;
 
 pub struct ParseResult {
-    _warnings: Warnings,
     tree: Owned<StmtList>,
 }
 
@@ -83,22 +61,5 @@ impl fmt::Debug for ParseResult {
         f.debug_struct("ParseResult")
             .field("tree", &**self)
             .finish_non_exhaustive()
-    }
-}
-
-struct Warnings {
-    stderr_buffer: Option<ptr::NonNull<ffi::c_char>>,
-}
-
-impl Drop for Warnings {
-    fn drop(&mut self) {
-        // tree was created with palloc, so is managed by postgres.
-        // stderr_buffer was malloc'd and must be freed
-        // SAFETY: libpg_query documents that the caller must free this.
-        unsafe {
-            if let Some(ptr) = self.stderr_buffer.take() {
-                libc::free(ptr.as_ptr() as _);
-            }
-        }
     }
 }
