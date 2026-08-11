2f4b8db732cc5597c08917f61182b8fd29524a4f
jkaczman <jkzgit@pm.me>
2026-08-10 21:37:32 -0400

    fix: give wrapped_raw_expression_tree_walker_impl external linkage

--- copy_pg_error.c
+++ copy_pg_error.c
@@ -1,4 +1,5 @@
 #include "copy_pg_error.h"
+#include "nodes/nodeFuncs.h"
 
 Error copy_pg_error() {
   MemoryContext mem = AllocSetContextCreate(
@@ -17,4 +18,15 @@ Error copy_pg_error() {
     .error_data = error_data
   };
   return error;
+}
+
+bool wrapped_raw_expression_tree_walker_impl(Node *n, tree_walker_callback w, void *c, Error *error) {
+  bool result;
+  PG_TRY();
+    result = raw_expression_tree_walker_impl(n, w, c);
+  PG_CATCH();
+    *error = copy_pg_error();
+    result = true;
+  PG_END_TRY();
+  return result;
 }
